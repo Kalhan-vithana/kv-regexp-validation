e62643eed15d0fcaa219c6e6fe92ea5e1823467c
P.V.D.M Kalhan vithana <kalhandushanka@gmail.com>
2023-04-04 21:43:17 +0530

    updated

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
-const  RegexValidator =  {
-    
-    RegexCSTValidator : (intput,mt= [{
-        min:1,
-        max:1,
-        leastOneletter:false,
-        leastOneNumber:false,
-        leastOnecharacter:false,
-        leastOneLowerCaseletter:false,
-        leastOneUpperCaseletter:false,
-        leastOneSpecialcharacter:false
-    }]) =>{
+const RegexValidator = {
+  RegexCSTValidator: (intput,mt = [
+      {
+        min: 1,
+        max: 1,
+        leastOneletter: false,
+        leastOneNumber: false,
+        leastOnecharacter: false,
+        leastOneLowerCaseletter: false,
+        leastOneUpperCaseletter: false,
+        leastOneSpecialcharacter: false,
+      },
+    ]
+  ) => {
+    let regex = new RegExp(
+      `${mt[0].leastOneLowerCaseletter ? `(?=.*[a-z])` : ""}^${
+        mt[0].leastOneUpperCaseletter ? `(?=.*[A-Z])` : ""
+      }${mt[0].leastOneletter ? `(?=.*[A-Za-z])` : ""}${
+        mt[0].leastOneNumber ? `(?=.*\\d)` : ""
+      }${
+        mt[0].leastOneSpecialcharacter ? `(?=.*[@$!%*#?&])` : ""
+      }[A-Za-z\\d@$!%*#?&]{${mt[0]?.min},${mt[0]?.max}}$`
+    );
 
-    
-    
-        let regex = new RegExp(`${mt[0].leastOneLowerCaseletter ? `(?=.*[a-z])` : ""}^${mt[0].leastOneUpperCaseletter ? `(?=.*[A-Z])` : ""}${mt[0].leastOneletter ? `(?=.*[A-Za-z])` : ""}${mt[0].leastOneNumber ? `(?=.*\\d)` :"" }${  mt[0].leastOneSpecialcharacter ? `(?=.*[@$!%*#?&])` : ""}[A-Za-z\\d@$!%*#?&]{${mt[0]?.min},${mt[0]?.max}}$`);
-        
-        return regex.test(intput);
-      
-    
-    },
+    return regex.test(intput);
+  },
 
-     RegexEMValidator : (input)=>{
-        
-        let regex = /^\w+@\w+\.\w{2,3}$/;
-        
-       return regex.test(input)
-    },
+  RegexEMValidator: (input) => {
+    let regex = /^\w+@\w+\.\w{2,3}$/;
 
-    RegexSTRCaseValidator :(caseString="",input="")=>{
-        
-        let regex = new RegExp("\\b" + caseString + "\\b")
-        
-        return regex.test(input)
-    }
+    return regex.test(input);
+  },
 
+  RegexSTRCaseValidator: (caseString = "", input = "") => {
+    let regex = new RegExp("\\b" + caseString + "\\b");
 
-
-}
+    return regex.test(input);
+  },
+};
 
 module.exports = RegexValidator;
-
